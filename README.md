80b07ace650ae06e502282cfd15e6e33529397d8
Fred Abo Setta <fabos016@uottawa.ca>
2024-11-10 20:36:47 -0500

    Add unit tests for the server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Fred Abo Setta 300160311
 
+**Unit tests are provided only for the server branch as the client one is unchanged**
+
 # employeeGql
--- src/test/kotlin/seg3x02/employeeGql/EmployeeGqlApplicationTests.kt
+++ src/test/kotlin/seg3x02/employeeGql/EmployeeGqlApplicationTests.kt
@@ -3,11 +3,51 @@ package seg3x02.employeeGql
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class EmployeeGqlApplicationTests {
+import org.junit.jupiter.api.Assertions
+import org.springframework.beans.factory.annotation.Autowired
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeeRepository
 
-	// @Test
-	// fun contextLoads() {
-	// }
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import seg3x02.employeeGql.resolvers.EmployeesController
+import org.springframework.data.mongodb.core.MongoOperations
+
+@SpringBootTest
+class EmployeeGqlApplicationTests @Autowired constructor(
+    val mongoOperations: MongoOperations
+) {
+
+	@Test
+	fun contextLoads() {
+	}
+
+	@Autowired
+    lateinit var employeeRepository: EmployeeRepository
+
+	@Test
+    fun `test the initialization of the employee entity`() {
+        val employee = Employee("John Smith", "2024-11-20", "Ottawa", 1000.0f, "M", "another@email.com")
+
+        Assertions.assertEquals("John Smith", employee.name)
+    }
+
+    @Test
+    fun `test the save and find functions of the employee repository`() {
+        val employee = Employee("John Smith", "2024-11-20", "Ottawa", 1000.0f, "M", "another@email.com")
+        val employeeId = employeeRepository.save(employee).employeeId
+
+        val repoEmployee = employeeRepository.findById(employeeId)
+        Assertions.assertTrue(repoEmployee.isPresent)
+    }
+
+	@Test
+    fun `test the write operation to the database`() {
+        val employee = CreateEmployeeInput("John Smith", "2024-11-20", "Ottawa", 1000.0f, "M", "another@email.com")
+		var employeeController = EmployeesController(mongoOperations, employeeRepository)
+
+        val dbEmployee = employeeController.newEmployee(employee)
+
+        Assertions.assertTrue(employeeController.employees().contains(dbEmployee))
+    }
 
 }
